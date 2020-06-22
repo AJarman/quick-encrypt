894339f57a97e44ee65e00def574e6baa9c971cd
AJarman <andrew.r.jarman@gmail.com>
2020-06-22 14:38:02 +0100

    Not much updated

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import os
 
 
 class DictoCrypto:
-    
+
     """
     Wrapper class to hold encryption and decryption methods.
-    
-    """
-        
-    @staticmethod
-    def encrypt_data(data:str or dict, 
-                           save_path:str=None, 
-                           description:str=None)->None:
 
+    """
+
+    @staticmethod
+    def encrypt_data(data: str or dict,
+                     save_path: str = None,
+                     description: str = None) -> None:
         """
         Takes some data (dict or str), and generates a fernet key.
         It then encrypts the data using the key, and saves the data 
@@ -25,10 +24,9 @@ class DictoCrypto:
         using the description to name the file.
 
 
-        Parameters:
+        Args:
         data (str or dict): Data to be encrypted, like an api key, or
         set of other credentials that you don't wish to share.
-
 
         save_path(str, optional): Must be a valid path or will raise
         OSError. Otherwise uses current working directory.
@@ -42,65 +40,59 @@ class DictoCrypto:
         """
 
         if save_path is None:
-            #use current working directory if not given
+            # use current working directory if not given
             save_path = os.getcwd()
         else:
-            #check if the path is valid and raise Error if not.
+            # check if the path is valid and raise Error if not.
             if not os.path.isdir(save_path):
                 raise OSError("No such path.")
 
-
         if description is None:
-            #use a boring obvious name!
+            # use a boring obvious name!
             description = 'secretfile'
 
         if isinstance(data, dict):
-            #convert to string/JSON if data is a dict.
+            # convert to string/JSON if data is a dict.
             data = json.dumps(data)
 
-
-
         # Generate a Fernet key
-        key = Fernet.generate_key() 
+        key = Fernet.generate_key()
         my_fernet = Fernet(key)
 
         # encrypt the data
         encrypted_data = my_fernet.encrypt(
-            bytes(data,encoding='utf-8'))
+            bytes(data, encoding='utf-8'))
 
-        #Write encryption key to file
-        key_path = f'{save_path}' +'/'+f'{description}-key.txt'
-        data_path = f'{save_path}' +'/'+f'{description}.txt'
+        # Write encryption key to file
+        key_path = f'{save_path}' + '/'+f'{description}-key.txt'
+        data_path = f'{save_path}' + '/'+f'{description}.txt'
 
         with open(key_path, 'wb') as f:
             f.write(key)
             f.close()
         print(key_path, "\n saved successfully")
 
-        #write encrypted file to file
+        # write encrypted file to file
         with open(data_path, 'wb') as f:
             f.write(encrypted_data)
             f.close()
         print(data_path, "\n saved successfully")
 
-
-    
-            
     @staticmethod
-    def decrypt_data(key_filepath:str, 
-                     encrypted_filepath:str)->dict or str:
+    def decrypt_data(key_filepath: str,
+                     encrypted_filepath: str) -> dict or str:
         """
         Takes an encrypted data file, unencrypts and returns it.
         (encrypted by the encrypt data method in this class)
-        
+
 
 
         Parameters:
-        
+
         encrypted_filepath(str): path to a fernet key, has to be 
         utf-8
-        
-        
+
+
         encrypted_filepath(str): path to a file to decrypt, has to be
         utf-8 encoded string, and encrypted using the key within the 
 
@@ -109,11 +101,11 @@ class DictoCrypto:
         Returns (dict or str):
         unencrypted data, will attempt to parse as json and return a
         string if this fails.
-        
+
 
         """
 
-        def open_as_bytestring(path:str)->bytes:
+        def open_as_bytestring(path: str) -> bytes:
             """ opens a text file or similar
             in read-only mode and parses it
             to bytes in utf-8 encoding
@@ -124,20 +116,19 @@ class DictoCrypto:
             Returns:
                 bytes: the contents of the file
                 as utf-8 bytes
-            """            
+            """
             with open(path, "r") as f:
                 return bytes(f.read(), 'utf-8')
 
-
         my_key = open_as_bytestring(path=key_filepath)
         my_file = open_as_bytestring(path=encrypted_filepath)
-        
+
         my_fernet = Fernet(my_key)
-        
+
         data = my_fernet.decrypt(my_file).decode('utf-8')
         try:
-            #attempt to convert to dictionary
+            # attempt to convert to dictionary
             return json.loads(data)
-        except JSONDecodeError:
-            #if it falls
+        except json.JSONDecodeError:
+            # if it falls
             return data
